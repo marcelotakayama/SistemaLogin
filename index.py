8a7d85a5c5f81015c5063ce3fdb5825f31683e7b
“marcelotakayama” <marcelotakayama00@gmail.com>
2020-01-13 13:14:58 -0300

    Algumas modificações no index, tudo funcionando

--- index.py
+++ index.py
@@ -8,4 +8,10 @@ jan.geometry("600x300")
 jan.configure(background="white")
 jan.resizable(width=False, height=False)
 
+LeftFrame = Frame(jan, width=200, height=300, bg="MIDNIGHTBLUE", relief="raise")
+LeftFrame.pack(side=LEFT)
+
+RightFrame = Frame(jan, width=200, height=300, bg="MIDNIGHTBLUE", relief="raise")
+
+
 jan.mainloop()
